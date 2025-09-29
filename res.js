1ce0980809e0ece96bf8e6983e01bbbf685b513a
Sallai András <andrewteki@gmail.com>
2025-09-29 13:05:22 +0200

    Read stdin

--- res.js
+++ res.js
@@ -8,73 +8,92 @@ const { convertToObject } = require('./comp/tools');
 const { put } = require('./comp/putmethod');
 const { patch } = require('./comp/patchmethod');
 
-const program = new Command();
+var inputData = {};
 
-program
-    .name('res')
-    .description('Command-line HTTP client')
-    .version('0.11.3')
+if(!process.stdin.isTTY) {
+  process.stdin.on('data', (content) => {
+    inputData = JSON.parse(content.toString().trim())
+    handler()
+  })
+} else {
+    handler()       
+}
 
-program
-    .argument('<url>', 'Server url')
-    .option('-a, --auth <token>', 'Token')
-    .option('-A, --auth-type <type>', 'The authentication mechanism. Currently only: bearer')
-    .parse()
-    .action((url, options) => {
-        get(url, options)
-    })
+function handler() {
+    const program = new Command();
 
-program
-    .command('get <url>')
-    .description('GET method')
-    .option('-a, --auth <token>', 'Token')
-    .option('-A, --auth-type <type>', 'The authentication mechanism. Currently only: bearer')
-    .parse()
-    .action((url, options) => {
-        get(url, options)
-    })
+    program
+        .name('res')
+        .description('Command-line HTTP client')
+        .version('0.12.0')
 
-program
-    .command('post <url> [params...]')
-    .description('POST method. The params: Key=value pairs, separated by spaces.')
-    .option('-a, --auth <token>', 'Token')
-    .option('-A, --auth-type <type>', 'The authentication mechanism. Currently only: bearer')
-    .parse()
-    .action((url, params, options) => {
-        const data = convertToObject(params)
-        post(url, data, options)
-    })
+    program
+        .argument('<url>', 'Server url')
+        .option('-a, --auth <token>', 'Token')
+        .option('-A, --auth-type <type>', 'The authentication mechanism. Currently only: bearer')
+        .parse()
+        .action((url, options) => {
+            get(url, options)
+        })
 
-program
-    .command('put <url> [params...]')
-    .description('PUT method')
-    .option('-a, --auth <token>', 'Token')
-    .option('-A, --auth-type <type>', 'The authentication mechanism. Currently only: bearer')
-    .parse()
-    .action((url, params, options) => {
-        const data = convertToObject(params)
-        put(url, data, options)
-    })
+    program
+        .command('get <url>')
+        .description('GET method')
+        .option('-a, --auth <token>', 'Token')
+        .option('-A, --auth-type <type>', 'The authentication mechanism. Currently only: bearer')
+        .parse()
+        .action((url, options) => {
+            get(url, options)
+        })
 
-program
-    .command('patch <url> [params...]')
-    .description('PATCH method')
-    .option('-a, --auth <token>', 'Token')
-    .option('-A, --auth-type <type>', 'The authentication mechanism. Currently only: bearer')
-    .parse()
-    .action((url, params, options) => {
-        const data = convertToObject(params)
-        patch(url, data, options)
-    })
+    program
+        .command('post <url> [params...]')
+        .description('POST method. The params: Key=value pairs, separated by spaces.')
+        .option('-a, --auth <token>', 'Token')
+        .option('-A, --auth-type <type>', 'The authentication mechanism. Currently only: bearer')
+        .parse()
+        .action(async (url, params, options) => {
+            if(!Object.keys(inputData).length) {
+                inputData = convertToObject(params)
+            }
+            post(url, inputData, options)
+        })
 
-program
-    .command('delete <url>')
-    .description('DELETE method')
-    .option('-a, --auth <token>', 'Token')
-    .option('-A, --auth-type <type>', 'The authentication mechanism. Currently only: bearer')
-    .parse()
-    .action((url, options) => {
-        del(url, options)
-    })
+    program
+        .command('put <url> [params...]')
+        .description('PUT method')
+        .option('-a, --auth <token>', 'Token')
+        .option('-A, --auth-type <type>', 'The authentication mechanism. Currently only: bearer')
+        .parse()
+        .action((url, params, options) => {
+            if(!Object.keys(inputData).length) {
+                inputData = convertToObject(params)
+            }
+            put(url, inputData, options)
+        })
 
-program.parse()
+    program
+        .command('patch <url> [params...]')
+        .description('PATCH method')
+        .option('-a, --auth <token>', 'Token')
+        .option('-A, --auth-type <type>', 'The authentication mechanism. Currently only: bearer')
+        .parse()
+        .action((url, params, options) => {
+            if(!Object.keys(inputData).length) {
+                inputData = convertToObject(params)
+            }
+            patch(url, inputData, options)            
+        })
+
+    program
+        .command('delete <url>')
+        .description('DELETE method')
+        .option('-a, --auth <token>', 'Token')
+        .option('-A, --auth-type <type>', 'The authentication mechanism. Currently only: bearer')
+        .parse()
+        .action((url, options) => {
+            del(url, options)
+        })
+
+    program.parse()
+}
